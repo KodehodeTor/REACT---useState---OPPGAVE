045dc7880eeb94a55b20b08ee63c220d9c62f181
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-10 14:37:11 +0200

    Added copy paste structure from useEffect() lesson to be amended later into the Cat Ninja Facts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
 import { useState } from "react";
 import "./App.css";
 
+//Cat API thingie
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
 
-  return <></>;
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        // Fetch API
+        const response = await fetch("https://catfact.ninja/facts?limit=5");
+        // If response is not OK throw error message
+        if (!response.ok) {
+          throw new Error(`HTTP error. Status: ${response.status}`);
+        }
+        // pauses until the data is requested fully and converted to JS
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        // Catch errors
+        setError(err.message);
+      } finally {
+        // Stop loading
+        setLoading(false);
+      }
+    };
+    // Fetch data
+    fetchData();
+    // Empty dependency array
+  }, []);
+
+  // Return
+  return (
+    <>
+      {/* If loading */}
+      {loading && <h1>Loading...</h1>}
+      {/* If errors */}
+      {error && <h2 style={{ color: "red" }}>Error: {error.message}</h2>}
+      {/* Append data */}
+      data && (
+      <div>
+        <h2>{data.title}</h2>
+        <p>Post id: {data.id}</p>
+      </div>
+      )
+    </>
+  );
 }
-
+// Export function
 export default App;
