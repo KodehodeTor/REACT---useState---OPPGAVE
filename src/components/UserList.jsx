cbb0f9e095c06572bebdf4e5b6d3813207f7fac9
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-16 09:50:55 +0200

    Renamed MockData.jsx to UserList.jsx

---
+++ src/components/UserList.jsx
@@ -0,0 +1,51 @@
+import { mockData } from "../data/mockData.js";
+import { useState } from "react";
+
+export function UserList() {
+  const [];
+}
+
+// export const mockData = [
+//   { username: "Ola Normann", email: "ola.normann@norge.no" },
+//   { username: "Torleif", email: "torleif@kodehode.no" },
+//   { username: "Jan Egil", email: "jan.egil@kodehode.no" },
+//   { username: "Sander", email: "sander@kodehode.no" },
+// ];
+
+// Psuedo code for list
+
+// <input type=txt value=username onchange={(e) => setUsername(e.target.value)} />
+//
+// const newUser = {info}
+//
+//  setUser ((prev) => [...prev] somethingsomething)
+//
+// {user.map((user) => ( div p username /p p email /p div ))}
+//
+// Date.now
+//
+// const userList = () => {
+// const [user, setUser] = useState ([])
+// const [username, setUsername] = useState ("")
+// const [email, setEmail] = useState("")
+//
+// const addUser = () => {
+//
+// create new user
+//
+// add new user
+//
+// Clear username and email input
+
+// return (
+//   <div>
+//     {/* username input */}
+//     {/* email input */}
+//     <button onClick={addUser}>Add User</button>
+//     {/* .map(user) */}
+//   </div>
+// )
+
+// export default userList
+
+// type → setUsername / setEmail → click → create object → setUsers → React rerenders → .map() displays updated array
